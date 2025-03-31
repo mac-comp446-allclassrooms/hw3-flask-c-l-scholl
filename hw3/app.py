e2b4d0a113570bddbe1d70345cd29d0186af2843
Camden Scholl <89877319+c-l-scholl@users.noreply.github.com>
2025-03-31 11:46:30 -0500

    adding html files

--- hw3/app.py
+++ hw3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
